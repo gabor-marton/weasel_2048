25319e73daeb5ae006df5f0e548d7b6cffbf2af7
denagyantal <denagyantal@gmail.com>
2019-05-22 21:35:35 +0200

    random error

--- play_2048.py
+++ play_2048.py
@@ -2,7 +2,18 @@ import requests
 import multiprocessing as mp
 
 # Current available algorithms
-from algorithms import alg_random, alg_random_half, db_expectimax, mc
+from algorithms import alg_random, alg_random_half, db_expectimax, mc, sneakyai
+from util import evaluate
+
+official_url = "https://thegame-2048.herokuapp.com"
+testing_url = "http://localhost:5000"
+
+DEBUG = False
+
+if DEBUG:
+    base_URL = testing_url
+else:
+    base_URL = official_url
 
 # Number of concurrent sessions/games
 TEST_NUMBER = 1
@@ -10,11 +21,10 @@ TEAM_NAME = "menyétek"
 maps = {}
 
 # Initiate algorithm configuration
-BEST_ALG = db_expectimax.db_expectimax
+BEST_ALG = sneakyai.algorithm
 
 # Algorithm chooser
 applied_algs = [BEST_ALG] * TEST_NUMBER
-
 
 uIds = [0] * TEST_NUMBER
 
@@ -22,22 +32,27 @@ uIds = [0] * TEST_NUMBER
 def initiate_game(table_index):
     """
     Initializes a new game instance on the server. Refreshes local data to apply the returned game board.
-
     :param table_index: The ID of the active game slot
     :return: None
     """
+    if DEBUG:
+        print("***** DEBUG MODE IS ON *****")
+
     try:
         # TODO: "random" should be the name of the algorithm
-        SESSION_NAME = TEAM_NAME + "_" + "captian_slow_and_deep"
+        SESSION_NAME = TEAM_NAME + "_" + applied_algs[table_index].label
 
-        request = requests.post(url="https://thegame-2048.herokuapp.com/api/new_game",
-                                json={"team_name": SESSION_NAME})
+        if DEBUG:
+            request = requests.get(url=base_URL + "/api/new_game")
+        else:
+            request = requests.post(url=base_URL + "/api/new_game",
+                                    json={"team_name": SESSION_NAME})
 
-        print(f"Start game on table {table_index}")
+        print(f"Start game on table {table_index} as {SESSION_NAME}")
         print(request.text)
 
-        maps[table_index] = request.json()
-        uIds[table_index] = maps[table_index]['uId']
+        maps[table_index] = request.json()['board']
+        uIds[table_index] = request.json()['uId']
         request.close()
 
     except Exception as e:
@@ -49,9 +64,7 @@ def initiate_game(table_index):
 def start_game(table_index):
     """
     Starts a game play on the table_index-th board.
-
     The game will be self-healing: if game_over is True, then initializes a new board and starts to play.
-
     :param table_index: the index of the concurrent boards
     :return: None
     """
@@ -59,19 +72,28 @@ def start_game(table_index):
 
     uId = uIds[table_index]
     current_map = maps[table_index]
-    current_alg = applied_algs[table_index]
+    current_alg = applied_algs[table_index].func
     game_over = False
 
     while True:
         if not game_over:
+            # print('Move scores')
+            # print(evaluate.evaluate(current_map, 0))
+            # print(evaluate.evaluate(current_map, 1))
+            # print(evaluate.evaluate(current_map, 2))
+            # print(evaluate.evaluate(current_map, 3))
+
             move = current_alg(current_map)
             print(move)
-            request = requests.post(url="https://thegame-2048.herokuapp.com/api/play_the_game",
+
+            request = requests.post(url=base_URL + "/api/play_the_game",
                                     json={'direction': move,
                                           'uId': uId})
 
+            current_map = request.json()['board']
+
             print(request.json())
-            # print(game_over)
+
             # TODO: Type checking, error handling (HTTP response?)
             game_over = request.json()["game_over"]
 
@@ -86,4 +108,4 @@ def start_game(table_index):
 pool = mp.Pool(mp.cpu_count())
 results = pool.map(start_game, range(TEST_NUMBER))
 pool.close()
-print(results[:10])
+print(results)
